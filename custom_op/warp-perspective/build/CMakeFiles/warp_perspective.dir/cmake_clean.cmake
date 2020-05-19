7a336e8a1a4f9406c2450b027056ab81314b66c6
masaru3624 <masaru3624@users.noreply.github.com>
2020-05-19 19:31:13 +0900

    add torch custom op exapmle

---
+++ custom_op/warp-perspective/build/CMakeFiles/warp_perspective.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/warp_perspective.dir/op.cpp.o"
+  "libwarp_perspective.pdb"
+  "libwarp_perspective.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/warp_perspective.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
